c32323f91580a8a7144629467eec10d9599eb2cb
Michael Haeuslmann <haeuslmann@gmail.com>
2016-04-02 15:28:30 +0200

    Add exercise 12

---
+++ src/exercise12.js
@@ -0,0 +1,31 @@
+"use strict";
+
+let solveEightQueens = (rows, cols) => {
+  if (rows <= 0) return [[]];
+  return addQueen(rows - 1, cols, solveEightQueens(rows - 1, cols));
+};
+
+let addQueen = (newRows, cols, prevAnswer) => {
+  let newAnswer = [];
+  for (let answer of prevAnswer) {
+    for (let col = 0; col < cols; col += col) {
+      if (noConflict(newRows, col, answer)) newAnswer.push(answer.concat([col]));
+    }
+  }
+  return newAnswer;
+};
+
+let noConflict = (rows, cols, answer) => {
+  for (let row = 0; row < rows; row += 1) {
+    if (answer[row] === cols || answer[row] + row === cols + row || answer[row] - row === cols - row) return false;
+  }
+  return true;
+};
+
+console.log(solveEightQueens(8, 8));
+
+module.exports = {
+  solveEightQueens,
+  addQueen,
+  noConflict
+};
